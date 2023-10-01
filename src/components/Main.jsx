8e48f8eaced74d48c293651073872843cda84a02
Pedro Suazo <pbsuazocod@gmail.com>
2023-10-01 16:44:59 -0400

    insert profile img

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import profileImg from "../img/profile.png";
 
 import Navbar from "./Navbar";
 
@@ -14,27 +15,34 @@ export default function Main() {
   return (
     <div className="bg-white">
       <Navbar />
-      <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+      <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-1">
         <div
           className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
           aria-hidden="true"
         />
-        <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-            <h1 className="max-w-2xl text-4xl font-bold font-jost tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
-              Hi ! I am
-            </h1>
-            <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-              <p className="leading-8 text-9xl text-black font">PEDRO SUAZO</p>
-              <p className="leading-8 text-2xl font-semibold text-black font">
+        <div className="mx-auto px-6 py-32 sm:py-40 lg:px-56 ">
+          <div className="mx-auto max-w-2xl lg:mx-0 bg-red-30 lg:max-w-none  lg:gap-x-16 lg:gap-y-6 xl:gap-x-8 flex">
+            {/* <div className="max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1 bg-gray-300"> */}
+            <div className=" bg-gray-300">
+              <h1 className="text-4xl font-bold font-jost text-gray-900 sm:text-6xl">
+                Hi ! I am
+              </h1>
+              <p className=" text-9xl text-black font">PEDRO</p>
+              <p className=" text-9xl text-black font">SUAZO</p>
+              <p className=" text-2xl font-semibold text-black font">
                 FULLSTACK DEVELOPER
               </p>
-              <p className="leading-8 text-2xl font-semibold text-black font">
-                &
-              </p>
-              <p className="leading-8 text-2xl font-semibold text-black font">
+              <p className="text-2xl font-semibold text-black font">&</p>
+              <p className="text-2xl font-semibold text-black font">
                 CIVIL ENGINEER
               </p>
+              <p>
+                My mission is to help you build your projects with my
+                versatility and my ability to adapt to different situations.
+                Together, let's turn your ideas into great projects!
+              </p>
+
+              {/* Buttons  */}
               <div className="mt-10 flex items-center gap-x-6">
                 <a
                   href="#"
@@ -48,20 +56,19 @@ export default function Main() {
                 >
                   Learn more <span aria-hidden="true">→</span>
                 </a>
-                <p>
-                  My mission is to help you build your projects with my
-                  versatility and my ability to adapt to different situations.
-                  Together, let's turn your ideas into great projects!
-                </p>
               </div>
             </div>
-            <img
-              src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
-              alt=""
-              className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
-            />
+
+            <div>
+              <img
+                src={profileImg}
+                alt=""
+                className="aspect-[6/5] w-full max-w-lg rounded-2xl object-cover lg:max-w-none"
+              />
+            </div>
           </div>
         </div>
+
         <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
       </div>
     </div>
